4946ff5cc2023f7010daecabd979b4b0b146e17d
Krzysiek <viruss.snk@gmail.com>
2017-03-08 12:22:59 +0100

    quality surveys still alpha

--- src/main/java/sbs/controller/qualitysurveys/QualitySurveysController.java
+++ src/main/java/sbs/controller/qualitysurveys/QualitySurveysController.java
@@ -45,7 +45,7 @@ public class QualitySurveysController {
 	QualitySurveysService surveysService;
 	@Autowired
 	QualitySurveyParameterAnswersService answersService;
-	@Autowired 
+	@Autowired
 	UserService userService;
 	@Autowired
 	MessageSource messageSource;
@@ -56,11 +56,13 @@ public class QualitySurveysController {
 	}
 
 	/*
-	 TODO
-	 	@RequestMapping(value = "/list")
-	 	@RequestMapping(value = "/view")
+	 * TODO
+	 * 
+	 * @RequestMapping(value = "/list")
+	 * 
+	 * @RequestMapping(value = "/view")
 	 */
-	
+
 	@RequestMapping(value = "/create")
 	public String selectUser(Model model, Locale locale) {
 		sessionForm = new QualitySurveySessionForm();
@@ -166,23 +168,10 @@ public class QualitySurveysController {
 		return "qualitysurveys/parametersurvey";
 	}
 
-	@RequestMapping(value = "/create", params = { "finishParametersSurvey" }, method = RequestMethod.POST)
-	@Transactional
-	public String finishParametersSurvey(ParameterSurveyForm parameterSurveyForm, BindingResult bindingResult,
-			Locale locale, Model model) {
-
-		System.out.println(sessionForm);
-		System.out.println(parameterSurveyForm);
-
-		ParameterSurveyItem psi;
-		QualitySurvey survey;
-		QualitySurveyParameter psp;
-		QualitySurveyParameterAnswer psa;
-		
-		survey = new QualitySurvey();
+	private void prepareSurveyEntityFrom(QualitySurvey survey, QualitySurveySessionForm sessionForm) {
 		survey.setUser(userService.getAuthenticatedUser());
 		survey.setCreationDate(new Timestamp(new java.util.Date().getTime()));
-		survey.setType(sessionForm.getType());		
+		survey.setType(sessionForm.getType());
 		survey.setClientCode(sessionForm.getClientCode());
 		survey.setClientName(sessionForm.getClientName());
 		survey.setProductCode(sessionForm.getProductCode());
@@ -195,95 +184,113 @@ public class QualitySurveysController {
 		survey.setOperatorLastName(sessionForm.getOperatorLastName());
 		survey.setOperatorId(sessionForm.getOperatorId());
 		survey.setOperatorRcpNo(sessionForm.getOperatorRcpNo());
-		
+	}
+
+	@RequestMapping(value = "/create", params = { "finishParametersSurvey" }, method = RequestMethod.POST)
+	@Transactional
+	public String finishParametersSurvey(ParameterSurveyForm parameterSurveyForm, BindingResult bindingResult,
+			Locale locale, Model model) {
+
+		System.out.println(sessionForm);
+		System.out.println(parameterSurveyForm);
+
+		ParameterSurveyItem psi;
+		QualitySurvey survey;
+		QualitySurveyParameter psp;
+		QualitySurveyParameterAnswer psa;
+
+		survey = new QualitySurvey();
+		prepareSurveyEntityFrom(survey, sessionForm);
+
 		ArrayList<QualitySurveyParameterAnswer> answers = new ArrayList<>();
-		
+
 		for (int i = 0; i < parameterSurveyForm.items.size(); i++) {
 			psi = parameterSurveyForm.items.get(i);
-			
-			switch (psi.getType()) {
-			case QualitySurveyParameter.PARAMETER_BOOLEAN:
-				if (psi.isYesnoAnswer()) {
-					psa = new QualitySurveyParameterAnswer();
-					// store data answer
-					psa.setYesnoAnswer(psi.isYesnoAnswer());
-					psa.setComment(psi.getComment());
-					// clear unused
-					psa.setModelAnswer("");
-					psa.setValueAnswer("");
+			psa = new QualitySurveyParameterAnswer();
 
-					// survey one-to-many
-					psa.setSurvey(survey);
-					survey.getParameterAnswers().add(psa);
-					
-					// parameter one-to-many
-					psp = parametersService.findById(psi.getParameterId());
-					psa.setParameter(psp);
-					psp.getAnswers().add(psa);
-					
-					// to save 
-					answers.add(psa);
-					
-				}
-				break;
-			case QualitySurveyParameter.PARAMETER_TEXT:
-				
-				psi.setModelAnswer(psi.getModelAnswer().trim());
-				psi.setAnswer(psi.getAnswer().trim());
-				
-				if (psi.getModelAnswer().length() > 0 || psi.getAnswer().length() > 0){
-					if(psi.getModelAnswer().length() > 0 && psi.getAnswer().length() > 0){
-						
-						psa = new QualitySurveyParameterAnswer();
+			switch (psi.getType()) {
+				case QualitySurveyParameter.PARAMETER_BOOLEAN:
+					if (psi.isYesnoAnswer()) {
+	
 						// store data answer
-						psa.setYesnoAnswer(false);
-						// clear unused
+						psa.setYesnoAnswer(psi.isYesnoAnswer());
 						psa.setComment(psi.getComment());
-						psa.setModelAnswer(psi.getModelAnswer());
-						psa.setValueAnswer(psi.getAnswer());
+						// clear unused
+						psa.setModelAnswer("");
+						psa.setValueAnswer("");
+	
 						// survey one-to-many
 						psa.setSurvey(survey);
 						survey.getParameterAnswers().add(psa);
-						
+	
 						// parameter one-to-many
 						psp = parametersService.findById(psi.getParameterId());
 						psa.setParameter(psp);
 						psp.getAnswers().add(psa);
-						
+	
 						// to save
 						answers.add(psa);
-						
+	
 					}
-					else{
-						bindingResult.rejectValue("items[" + i + "].modelAnswer", "quality.surveys.error.both.values", "ERROR");
-						bindingResult.rejectValue("items[" + i + "].answer", "quality.surveys.error.both.values", "ERROR");
+				break;
+				case QualitySurveyParameter.PARAMETER_TEXT:
+	
+					psi.setModelAnswer(psi.getModelAnswer().trim());
+					psi.setAnswer(psi.getAnswer().trim());
+	
+					if (psi.getModelAnswer().length() > 0 || psi.getAnswer().length() > 0) {
+						if (psi.getModelAnswer().length() > 0 && psi.getAnswer().length() > 0) {
+	
+							// store data answer
+							psa.setYesnoAnswer(false);
+							// clear unused
+							psa.setComment(psi.getComment());
+							psa.setModelAnswer(psi.getModelAnswer());
+							psa.setValueAnswer(psi.getAnswer());
+							// survey one-to-many
+							psa.setSurvey(survey);
+							survey.getParameterAnswers().add(psa);
+	
+							// parameter one-to-many
+							psp = parametersService.findById(psi.getParameterId());
+							psa.setParameter(psp);
+							psp.getAnswers().add(psa);
+	
+							// to save
+							answers.add(psa);
+	
+						} else {
+							bindingResult.rejectValue("items[" + i + "].modelAnswer", "quality.surveys.error.both.values",
+									"ERROR");
+							bindingResult.rejectValue("items[" + i + "].answer", "quality.surveys.error.both.values",
+									"ERROR");
+						}
 					}
-				}
 				break;
 			}
 		}
 
-		if(survey.getParameterAnswers().size()<2){
+		if (survey.getParameterAnswers().size() < 2) {
 			bindingResult.reject("quality.surveys.error.not.enough.answers");
 		}
-		
+
 		if (bindingResult.hasErrors()) {
 			TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
 			return "qualitysurveys/parametersurvey";
 		}
-		
+
 		surveysService.save(survey);
-		for(QualitySurveyParameterAnswer psts: answers){
+		for (QualitySurveyParameterAnswer psts : answers) {
 			answersService.save(psts);
 		}
-		
+
 		QualitySurveySessionForm newSessionForm = new QualitySurveySessionForm();
 		newSessionForm.copyOperatorInfo(sessionForm);
 		sessionForm = newSessionForm;
 		model.addAttribute("productionOrderForm", new ProductionOrderForm());
-		
+
 		model.addAttribute("msg", messageSource.getMessage("action.saved", null, locale));
-		
+
 		return "qualitysurveys/surveydetails";
 	}
 
