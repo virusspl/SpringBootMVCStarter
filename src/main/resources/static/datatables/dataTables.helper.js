ca3f1c89dfc5369fe5aafa3770651fd156700952
Krzysiek <viruss.snk@gmail.com>
2017-03-15 07:32:01 +0100

    get json for datatables normal way

--- src/main/resources/static/datatables/dataTables.helper.js
+++ src/main/resources/static/datatables/dataTables.helper.js
@@ -2,17 +2,14 @@ function getUrl (){
 	
 	var baseUrl;
 	var finalUrl;
+	
+	var url = window.location.href;
+	var arr = url.split("/");
+	var result = arr[0] + "//" + arr[2];
+	if(arr[3] == "adr"){
+		result = result + "/adr";
+	}
+	result = result + "/datatables/";
 
-	if(window.location.host == "localhost:8080"){
-		baseUrl = "http://localhost:8080/datatables/"
-	}
-	else if (window.location.host == "atw-admin_3:8080"){
-		baseUrl = "http://atw-admin_3:8080/datatables/"
-	}
-	else{
-		baseUrl = "http://atwsrv-java:8080/adr/datatables/"
-	}
-	
-	return baseUrl;
-	
+	return result;
 }
