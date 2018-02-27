7257841937594cc5830f4834c5b0046c40eda914
Krzysiek <viruss.snk@gmail.com>
2018-02-27 07:43:56 +0100

    version number in footer

--- src/main/java/sbs/controller/aspect/GlobalControllerAdvice.java
+++ src/main/java/sbs/controller/aspect/GlobalControllerAdvice.java
@@ -1,6 +1,7 @@
 package sbs.controller.aspect;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
@@ -11,10 +12,17 @@ public class GlobalControllerAdvice {
 
 	@Autowired
 	LiveFeedSingleton lvs;
+	@Autowired
+	Environment env;
+	
 	
     @ModelAttribute("live")
     public String getLiveFeedMessage() {
         return lvs.getMessage();
     }
     
+    @ModelAttribute("build")
+    public String getBuildVersion() {
+        return env.getRequiredProperty("build.version");
+    }
 }
