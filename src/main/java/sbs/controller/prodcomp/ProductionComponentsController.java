56cde8fdd9ef13ef929b9618d561d19b348abfff
Krzysiek <viruss.snk@gmail.com>
2019-03-21 14:23:07 +0100

    prodcomp ready

--- src/main/java/sbs/controller/prodcomp/ProductionComponentsController.java
+++ src/main/java/sbs/controller/prodcomp/ProductionComponentsController.java
@@ -32,24 +32,12 @@ public class ProductionComponentsController {
 
 	@RequestMapping("/main")
 	public String view() {
-		// TODO clear
-		long start = System.currentTimeMillis();
-		// Map<String, List<X3BomItem>> allBoms =
-		// x3Service.getAllBomPartsTopLevel("ATW");
-		// System.out.println(allBoms.size());
-		// Map<String, Double> result =
-		// getComponentsQuantitiesMultilevel(allBoms, "GSK225095P5P");
-		// System.out.println(result);
-		//Map<String, String> descriptions = x3Service.getDescriptionsByLanguage(JdbcOracleX3Service.LANG_POLISH, "ATW");
-		//System.out.println(descriptions);
-		System.out.println((System.currentTimeMillis() - start) / 1000);
 		return "prodcomp/main";
 	}
 
 	@RequestMapping("/make")
 	public String doMake(Model model, Locale locale, RedirectAttributes redirectAttrs)
 			throws FileNotFoundException, IOException {
-		long start = System.currentTimeMillis();
 		// get file
 		File file = (File) model.asMap().get("file");
 		if (file != null) {
@@ -121,15 +109,24 @@ public class ProductionComponentsController {
 				}
 			}
 
-			model.addAttribute("components", allComponents);
+			Map<String, String> descriptions = x3Service.getDescriptionsByLanguage(JdbcOracleX3Service.LANG_POLISH, "ATW");
+			List<List<String>> table = new ArrayList<>();
+			List<String> line;
+			
+			for(Map.Entry<String, Double> entry: allComponents.entrySet()){
+				line = new ArrayList<>();
+				line.add(entry.getKey());
+				line.add(descriptions.containsKey(entry.getKey()) ? descriptions.get(entry.getKey()) : "-");
+				line.add(entry.getValue().toString());
+				table.add(line);
+			}
+			model.addAttribute("components", table);
 		} else {
 			// no file
 			redirectAttrs.addFlashAttribute("main", messageSource.getMessage("action.choose.file", null, locale));
 			return "redirect:/prodcomp/main";
 		}
 		
-		
-		System.out.println((System.currentTimeMillis() - start) / 1000);
 		return "prodcomp/main";
 	}
 
@@ -142,23 +139,17 @@ public class ProductionComponentsController {
 
 		String code;
 		double qty;
-		List<X3BomItem> subList;
 		Map<String, Double> subMap;
 
 		for (X3BomItem item : list) {
 			code = item.getPartCode();
 			qty = item.getModelQuantity();
 
-			subList = findBomPartsByParentCode(allBoms, code);
-			// x3Service.findBomPartsByParent("ATW", code);
-			// TODO delete if isempty, do this below 
-			if (subList.isEmpty()) {
-				if (resultMap.containsKey(code)) {
-					resultMap.put(code, resultMap.get(code) + qty);
-				} else {
-					resultMap.put(code, qty);
-				}
+			if (resultMap.containsKey(code)) {
+				resultMap.put(code, resultMap.get(code) + qty);
 			} else {
+				resultMap.put(code, qty);
+			}
 				subMap = getComponentsQuantitiesMultilevel(allBoms, code);
 				for (Map.Entry<String, Double> entry : subMap.entrySet()) {
 					if (resultMap.containsKey(entry.getKey())) {
@@ -168,7 +159,7 @@ public class ProductionComponentsController {
 						resultMap.put(entry.getKey(), item.getModelQuantity() * entry.getValue());
 					}
 				}
-			}
+			
 		}
 		return resultMap;
 	}
@@ -183,77 +174,4 @@ public class ProductionComponentsController {
 		return list;
 	}
 
-	/*
-	 * @RequestMapping(value = "/makelist", params = { "viewlist" }, method =
-	 * RequestMethod.POST) public String viewList(@Valid OrdersToolsForm
-	 * ordersToolsForm, BindingResult bindingResult, Model model,
-	 * RedirectAttributes redirectAttrs, Locale locale) { if
-	 * (bindingResult.hasErrors()) { return "ordtools/main"; }
-	 * 
-	 * // database list // Map<String, Integer> stock = //
-	 * x3Service.findGeneralStockForAllProducts("ATW"); List<X3SalesOrderItem>
-	 * lines =
-	 * x3Service.findAllSalesOrdersAfvItemsInPeriod(ordersToolsForm.getStartDate
-	 * (), ordersToolsForm.getEndDate(), "ATW");
-	 * 
-	 * List<X3KeyValString> allBomParts = x3Service.getAllBomPartsInBoms("ATW");
-	 * List<X3ToolEntry> allTools = x3Service.getAllToolsInRouting("ATW");
-	 * Map<String, Set<String>> bomPartsByCode = prepareMapByCode(allBomParts);
-	 * Map<String, List<X3ToolEntry>> toolsOperations =
-	 * prepareToolsOperations(allTools); // ^List<X3ToolEntry> // loop through
-	 * lines OrdersToolsLine line; List<OrdersToolsLine> list = new
-	 * ArrayList<>(); Set<X3ToolEntry> set;
-	 * 
-	 * 
-	 * for (X3SalesOrderItem sois : lines) { set = new HashSet<>();
-	 * fillEmptySetWithToolsByProductCode(sois.getProductCode(), set,
-	 * bomPartsByCode, toolsOperations); if (set.size() > 0) { for (X3ToolEntry
-	 * tool : set) { line = createOrdersToolsLineFromX3Info(sois);
-	 * line.setTool(tool); list.add(line); } } else { line =
-	 * createOrdersToolsLineFromX3Info(sois); line.setTool(new X3ToolEntry());
-	 * list.add(line); } }
-	 * 
-	 * model.addAttribute("list", list); model.addAttribute("startDate",
-	 * ordersToolsForm.getStartDate()); model.addAttribute("endDate",
-	 * ordersToolsForm.getEndDate());
-	 * 
-	 * return "ordtools/main"; }
-	 * 
-	 * private Map<String, Set<String>> prepareMapByCode(List<X3KeyValString>
-	 * pairs) { Map<String, Set<String>> map = new HashMap<>(); Set<String> set;
-	 * for (X3KeyValString pair : pairs) { if (map.containsKey(pair.getKey())) {
-	 * map.get(pair.getKey()).add(pair.getValue()); } else { set = new
-	 * HashSet<>(); set.add(pair.getValue()); map.put(pair.getKey(), set); } }
-	 * return map; }
-	 * 
-	 * private Map<String, List<X3ToolEntry>>
-	 * prepareToolsOperations(List<X3ToolEntry> toolsList) { Map<String,
-	 * List<X3ToolEntry>> map = new HashMap<>(); List<X3ToolEntry> list; for
-	 * (X3ToolEntry entry : toolsList) { if (map.containsKey(entry.getCode())) {
-	 * map.get(entry.getCode()).add(entry); } else { list = new ArrayList<>();
-	 * list.add(entry); map.put(entry.getCode(), list); }
-	 * 
-	 * } return map; }
-	 * 
-	 * private void fillEmptySetWithToolsByProductCode(String code,
-	 * Set<X3ToolEntry> set, Map<String, Set<String>> bomPartsByCode,
-	 * Map<String, List<X3ToolEntry>> toolsOperations) {
-	 * 
-	 * // Tools if (toolsOperations.containsKey(code)) { for (X3ToolEntry tool :
-	 * toolsOperations.get(code)) { set.add(tool); } }
-	 * 
-	 * // BOM if (bomPartsByCode.containsKey(code)) { for (String cpnitmref :
-	 * bomPartsByCode.get(code)) { fillEmptySetWithToolsByProductCode(cpnitmref,
-	 * set, bomPartsByCode, toolsOperations); } } }
-	 * 
-	 * private OrdersToolsLine createOrdersToolsLineFromX3Info(X3SalesOrderItem
-	 * sois) { OrdersToolsLine otl = new OrdersToolsLine();
-	 * otl.setOrder(sois.getOrderNumber());
-	 * otl.setDemandedDate(sois.getDemandedDate());
-	 * otl.setProductCode(sois.getProductCode());
-	 * otl.setProductDescription(sois.getProductDescription());
-	 * otl.setQuantity(sois.getQuantityOrdered()); return otl;
-	 * 
-	 * }
-	 */
 }
