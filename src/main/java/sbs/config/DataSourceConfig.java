68c77b3907c01d49378e59d287a20d7de22eb38d
Krzysiek <viruss.snk@gmail.com>
2019-05-06 08:27:56 +0200

    clear console of broker/sql

--- src/main/java/sbs/config/DataSourceConfig.java
+++ src/main/java/sbs/config/DataSourceConfig.java
@@ -55,7 +55,7 @@ public class DataSourceConfig {
 	public SessionFactory sessionFactory() {
 		DataSource dataSource = dataSource();
 		Properties properties = new Properties();
-		properties.put("hibernate.show_sql", "true");
+		properties.put("hibernate.show_sql", "false");
 		properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
 		properties.put("hibernate.hbm2ddl.auto", "update");
 		LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);	
